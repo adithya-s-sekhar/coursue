bc31c56e299f8f9aa58168008a55d2176d49e6c6
Adithya S Sekhar <adithya0332@gmail.com>
2023-12-15 01:41:40 +0530

    mentorheader

--- src/components/SidebarRight/SidebarRightProfile/SidebarRightProfileContent/SidebarRightProfileContentMentors/SidebarRightProfileContentMentorsHeader/SidebarRightProfileContentMentorsHeader.jsx
+++ src/components/SidebarRight/SidebarRightProfile/SidebarRightProfileContent/SidebarRightProfileContentMentors/SidebarRightProfileContentMentorsHeader/SidebarRightProfileContentMentorsHeader.jsx
@@ -3,7 +3,15 @@ import './SidebarRightProfileContentMentorsHeader.css';
 const SidebarRightProfileContentMentorsHeader = () => {
     return ( 
         <div className='sidebar-right-profile-content-mentors-header'>
-            
+            <div className='sidebar-right-profile-content-mentors-header-text'>
+                Your Mentor
+            </div>
+            <div className='sidebar-right-profile-content-mentors-header-button'>
+                <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12" fill="none">
+                    <path d="M3 6H9" stroke="#9E9E9E" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M6 9V3" stroke="#9E9E9E" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+            </div>
         </div>
      );
 }
