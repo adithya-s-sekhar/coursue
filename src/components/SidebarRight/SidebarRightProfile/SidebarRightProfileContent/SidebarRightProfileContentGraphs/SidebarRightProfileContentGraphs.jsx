0eb72ed1b62f696ef5e373c5c3ff3d6072833b59
Adithya S Sekhar <adithya0332@gmail.com>
2023-12-15 01:28:52 +0530

    graphs

--- src/components/SidebarRight/SidebarRightProfile/SidebarRightProfileContent/SidebarRightProfileContentGraphs/SidebarRightProfileContentGraphs.jsx
+++ src/components/SidebarRight/SidebarRightProfile/SidebarRightProfileContent/SidebarRightProfileContentGraphs/SidebarRightProfileContentGraphs.jsx
@@ -1,10 +1,48 @@
 import './SidebarRightProfileContentGraphs.css';
-import SidebarRightProfileContentGraphsBar from './SidebarRightProfileContentGraphsBar/SidebarRightProfileContentGraphsBar';
 
 const SidebarRightProfileContentGraphs = () => {
     return ( 
         <div className='sidebar-right-profile-content-graphs'>
-            <SidebarRightProfileContentGraphsBar />
+            <div className='sidebar-right-profile-content-graphs-item'>
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="41" viewBox="0 0 32 41" fill="none">
+                    <rect y="22.4432" width="31.2" height="18.5568" fill="#962DFF"/>
+                    <rect y="13.1648" width="31.2" height="9.27841" fill="#C893FD"/>
+                    <rect y="5.67218" width="31.2" height="7.49251" fill="#E0C6FD"/>
+                    <path d="M0 4.97748C0 2.76834 1.79086 0.977478 4 0.977478H27.2C29.4091 0.977478 31.2 2.76834 31.2 4.97748V5.67225H0V4.97748Z" fill="#F0E5FC"/>
+                </svg>
+            </div>
+            <div className='sidebar-right-profile-content-graphs-item'>
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="72" viewBox="0 0 32 72" fill="none">
+                    <rect x="0.199951" y="40.852" width="31.2" height="31.148" fill="#962DFF"/>
+                    <rect x="0.199951" y="25.7233" width="31.2" height="15.1285" fill="#C893FD"/>
+                    <rect x="0.199951" y="8.57172" width="31.2" height="17.1517" fill="#E0C6FD"/>
+                    <path d="M0.199951 4.29621C0.199951 2.08707 1.99081 0.296204 4.19995 0.296204H27.4C29.6091 0.296204 31.4 2.08707 31.4 4.2962V8.57175H0.199951V4.29621Z" fill="#F0E5FC"/>
+                </svg>
+            </div>
+            <div className='sidebar-right-profile-content-graphs-item'>
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="92" viewBox="0 0 32 92" fill="none">
+                    <rect x="0.399902" y="51.7938" width="31.2" height="40.2062" fill="#962DFF"/>
+                    <rect x="0.399902" y="33.1275" width="31.2" height="18.6664" fill="#C893FD"/>
+                    <rect x="0.399902" y="12.1172" width="31.2" height="21.0103" fill="#E0C6FD"/>
+                    <path d="M0.399902 4.72516C0.399902 2.51602 2.19076 0.725159 4.3999 0.725159H27.5999C29.809 0.725159 31.5999 2.51602 31.5999 4.72516V12.1172H0.399902V4.72516Z" fill="#F0E5FC"/>
+                </svg>   
+            </div>
+            <div className='sidebar-right-profile-content-graphs-item'>
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="109" viewBox="0 0 32 109" fill="none">
+                    <rect x="0.600098" y="62.2547" width="31.2" height="46.7453" fill="#962DFF"/>
+                    <rect x="0.600098" y="38.4021" width="31.2" height="23.8526" fill="#C893FD"/>
+                    <rect x="0.600098" y="12.2055" width="31.2" height="26.1965" fill="#E0C6FD"/>
+                    <path d="M0.600098 4.81353C0.600098 2.6044 2.39096 0.813538 4.6001 0.813538H27.8001C30.0092 0.813538 31.8001 2.6044 31.8001 4.81354V12.2056H0.600098V4.81353Z" fill="#F0E5FC"/>
+                </svg> 
+            </div>
+            <div className='sidebar-right-profile-content-graphs-item'>
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="95" viewBox="0 0 32 95" fill="none">
+                    <rect x="0.800049" y="52.8803" width="31.2" height="42.1197" fill="#962DFF"/>
+                    <rect x="0.800049" y="32.586" width="31.2" height="20.2944" fill="#C893FD"/>
+                    <rect x="0.800049" y="11.3898" width="31.2" height="21.1962" fill="#E0C6FD"/>
+                    <path d="M0.800049 4.042C0.800049 1.83286 2.59091 0.0419922 4.80005 0.0419922H28.0001C30.2092 0.0419922 32 1.83285 32 4.04199V11.3898H0.800049V4.042Z" fill="#F0E5FC"/>
+                </svg>
+            </div>
         </div>
      );
 }
