9cb2cdcb74921f0124b5d92d424f1dc53a73e303
Adithya S Sekhar <adithya0332@gmail.com>
2023-12-15 10:53:24 +0530

    upd

--- src/components/MainContent/MainContentMentors/MainContentMentorsList/MainContentMentorsList.jsx
+++ src/components/MainContent/MainContentMentors/MainContentMentorsList/MainContentMentorsList.jsx
@@ -1,9 +1,11 @@
 import './MainContentMentorsList.css';
+import MainContentMentorsListHeader from './MainContentMentorsListHeader/MainContentMentorsListHeader';
 import MainContentMentorsListItem from './MainContentMentorsListItem/MainContentMentorsListItem';
 
 const MainContentMentorsList = () => {
     return ( 
         <div className='main-content-mentors-list'>
+            <MainContentMentorsListHeader />
             <MainContentMentorsListItem />
         </div>
      );
