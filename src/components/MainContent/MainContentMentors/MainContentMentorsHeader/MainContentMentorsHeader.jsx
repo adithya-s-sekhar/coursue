aab80655024928fab272eb42b60fd15ffdbbf7c3
Adithya S Sekhar <adithya0332@gmail.com>
2023-12-15 10:45:10 +0530

    header

--- src/components/MainContent/MainContentMentors/MainContentMentorsHeader/MainContentMentorsHeader.jsx
+++ src/components/MainContent/MainContentMentors/MainContentMentorsHeader/MainContentMentorsHeader.jsx
@@ -3,7 +3,12 @@ import './MainContentMentorsHeader.css';
 const MainContentMentorsHeader = () => {
     return ( 
         <div className='main-content-mentors-header'>
-            
+            <div className='main-content-mentors-header-text'>
+                Your Mentor
+            </div>
+            <div className='main-content-mentors-header-see-all'>
+                <a href='#'>See All</a>
+            </div>
         </div>
      );
 }
