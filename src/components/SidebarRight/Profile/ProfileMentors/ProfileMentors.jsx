1e606a7861241f66ed0a69b49acb41fdd1912e04
Adithya S Sekhar <adithya0332@gmail.com>
2023-12-17 17:31:21 +0530

    styling: profile mentors

--- src/components/SidebarRight/Profile/ProfileMentors/ProfileMentors.jsx
+++ src/components/SidebarRight/Profile/ProfileMentors/ProfileMentors.jsx
@@ -20,33 +20,21 @@ const ProfileMentors = () => {
                     name = 'Prasant Kumar Singh'
                     job = 'Software Developer'
                 />
-                    <svg xmlns="http://www.w3.org/2000/svg" width="225" height="2" viewBox="0 0 225 2" fill="none">
-                        <path d="M224 1.25C224.138 1.25 224.25 1.13807 224.25 1C224.25 0.861929 224.138 0.75 224 0.75V1.25ZM0 1.25H224V0.75H0V1.25Z" fill="#D8D8D8"/>
-                    </svg>
                 <ProfileMentorsListItem 
                     image = 'prasant.jpg'
                     name = 'Prasant Kumar Singh'
                     job = 'Software Developer'
                 />
-                    <svg xmlns="http://www.w3.org/2000/svg" width="225" height="2" viewBox="0 0 225 2" fill="none">
-                        <path d="M224 1.25C224.138 1.25 224.25 1.13807 224.25 1C224.25 0.861929 224.138 0.75 224 0.75V1.25ZM0 1.25H224V0.75H0V1.25Z" fill="#D8D8D8"/>
-                    </svg>
                 <ProfileMentorsListItem 
                     image = 'prasant.jpg'
                     name = 'Prasant Kumar Singh'
                     job = 'Software Developer'
                 />
-                    <svg xmlns="http://www.w3.org/2000/svg" width="225" height="2" viewBox="0 0 225 2" fill="none">
-                        <path d="M224 1.25C224.138 1.25 224.25 1.13807 224.25 1C224.25 0.861929 224.138 0.75 224 0.75V1.25ZM0 1.25H224V0.75H0V1.25Z" fill="#D8D8D8"/>
-                    </svg>
                 <ProfileMentorsListItem 
                     image = 'prasant.jpg'
                     name = 'Prasant Kumar Singh'
                     job = 'Software Developer'
                 />
-                    <svg xmlns="http://www.w3.org/2000/svg" width="225" height="2" viewBox="0 0 225 2" fill="none">
-                        <path d="M224 1.25C224.138 1.25 224.25 1.13807 224.25 1C224.25 0.861929 224.138 0.75 224 0.75V1.25ZM0 1.25H224V0.75H0V1.25Z" fill="#D8D8D8"/>
-                    </svg>
                 <ProfileMentorsListItem 
                     image = 'prasant.jpg'
                     name = 'Prasant Kumar Singh'
--- src/components/SidebarRight/Profile/ProfileMentors/ProfileMentorsListItem/ProfileMentorsListItem.jsx
+++ src/components/SidebarRight/Profile/ProfileMentors/ProfileMentorsListItem/ProfileMentorsListItem.jsx
@@ -5,19 +5,23 @@ const ProfileMentorsListItem = (props) => {
 
     return ( 
         <div className='profile-mentors-list-item'>
-            <div className='profile-mentors-list-item-image-mask'>
-                <img src={image} />
-            </div>
             <div className='profile-mentors-list-item-details'>
-                <div className='profile-mentors-list-item-details-name'>
-                    {name}
+                <div className='profile-mentors-list-item-image-mask'>
+                    <img src={image} />
                 </div>
-                <div className='profile-mentors-list-item-details-job'>
-                    {job}
+                <div className="profile-mentors-list-item-text">
+                    <div className='profile-mentors-list-item-name'>
+                        {name}
+                    </div>
+                    <div className='profile-mentors-list-item-job'>
+                        {job}
+                    </div>
                 </div>
             </div>
             <div className='profile-mentors-list-item-follow'>
+                <div className='profile-mentors-list-item-follow-text'>
                     Follow
+                </div>
             </div>
         </div>
      );
