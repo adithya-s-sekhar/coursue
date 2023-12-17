94b8064f374bd01bafe4dc2a81a664fdc5dc2ccf
Adithya S Sekhar <adithya0332@gmail.com>
2023-12-17 09:29:39 +0530

    simplify: main content video

--- src/components/MainContent/MainContentVideos/MainContentVideoItem/MainContentVideoItem.jsx
+++ src/components/MainContent/MainContentVideos/MainContentVideoItem/MainContentVideoItem.jsx
@@ -1,6 +1,6 @@
-import './MainContentVideosListItem.css';
+import './MainContentVideoItem.css';
 
-const MainContentVideosListItem = (props) => {
+const MainContentVideoItem = (props) => {
     const thumb = props.thumb;
     const category = props.category;
     const title = props.title;
@@ -10,32 +10,32 @@ const MainContentVideosListItem = (props) => {
     const uploaderJob = props.uploaderJob;
 
     return ( 
-        <div className='main-content-videos-list-item'>
-            <img src={thumb} className='main-content-videos-list-item-thumb' />
-            <div className='main-content-videos-list-item-category'>
+        <div className='main-content-video-item'>
+            <img src={thumb} className='main-content-video-item-thumb' />
+            <div className='main-content-video-item-category'>
                 {category}
             </div>
-            <div className='main-content-videos-list-item-title'>
+            <div className='main-content-video-item-title'>
                 {title}
             </div>
-            <div className='main-content-videos-list-item-progress'>
-                <div className='main-content-videos-list-item-progress-completed' style={{width: `${progress}`}}>
+            <div className='main-content-video-item-progress'>
+                <div className='main-content-video-item-progress-completed' style={{width: `${progress}`}}>
                 </div>
             </div>
-            <div className='main-content-videos-list-item-uploader'>
-                <div className='main-content-videos-list-item-uploader-image-mask'>
+            <div className='main-content-video-item-uploader'>
+                <div className='main-content-video-item-uploader-image-mask'>
                     <img src={uploaderImage} />
                 </div>
-                <div className='main-content-videos-list-item-uploader-details'>
-                    <div className='main-content-videos-list-item-uploader-name'>
+                <div className='main-content-video-item-uploader-details'>
+                    <div className='main-content-video-item-uploader-name'>
                         {uploaderName}
                     </div>
-                    <div className='main-content-videos-list-item-uploader-job'>
+                    <div className='main-content-video-item-uploader-job'>
                         {uploaderJob}
                     </div>
                 </div>
             </div>
-            <div className='main-content-videos-list-item-heart'>
+            <div className='main-content-video-item-heart'>
                 <svg xmlns="http://www.w3.org/2000/svg" width="8" height="8" viewBox="0 0 8 8" fill="none">
                     <path d="M4.2065 6.93666C4.09317 6.97666 3.9065 6.97666 3.79317 6.93666C2.8265 6.60666 0.666504 5.22999 0.666504 2.89666C0.666504 1.86666 1.4965 1.03333 2.51984 1.03333C3.1265 1.03333 3.66317 1.32666 3.99984 1.77999C4.3365 1.32666 4.8765 1.03333 5.47984 1.03333C6.50317 1.03333 7.33317 1.86666 7.33317 2.89666C7.33317 5.22999 5.17317 6.60666 4.2065 6.93666Z" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
                 </svg>
@@ -44,4 +44,4 @@ const MainContentVideosListItem = (props) => {
      );
 }
  
-export default MainContentVideosListItem;
+export default MainContentVideoItem;
--- src/components/MainContent/MainContentVideos/MainContentVideos.jsx
+++ src/components/MainContent/MainContentVideos/MainContentVideos.jsx
@@ -1,12 +1,55 @@
 import './MainContentVideos.css';
-import MainContentVideosHeader from './MainContentVideosHeader/MainContentVideosHeader';
-import MainContentVideosList from './MainContentVideosList/MainContentVideosList';
+import MainContentVideoItem from './MainContentVideoItem/MainContentVideoItem';
 
 const MainContentVideos = () => {
     return ( 
         <div className='main-content-videos'>
-            <MainContentVideosHeader />
-            <MainContentVideosList />
+            <div className='main-content-videos-header'>
+                <div className='main-content-videos-header-text'>
+                    Continue Watching
+                </div>
+                <div className='main-content-videos-header-btns'>
+                    <div className='main-content-videos-header-btns-item'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12" fill="none">
+                            <path d="M7.49992 9.96001L4.23992 6.70001C3.85492 6.31501 3.85492 5.68501 4.23992 5.30001L7.49992 2.04001" stroke="#9E9E9E" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                    </div>
+                    <div className='main-content-videos-header-btns-item'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12" fill="none">
+                            <path d="M4.45508 9.96001L7.71508 6.70001C8.10008 6.31501 8.10008 5.68501 7.71508 5.30001L4.45508 2.04001" stroke="#9E9E9E" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                    </div>
+                </div>
+            </div>
+            <div className='main-content-videos-list'>
+                <MainContentVideoItem 
+                    thumb = 'thumb1.jpg'
+                    category = 'Frontend'
+                    title = 'Beginners Guide To Becoming A Professional Frontend Developer'
+                    progress = '50%'
+                    uploaderImage = 'prasant.jpg'
+                    uploaderName = 'Prasant Kumar Singh'
+                    uploaderJob = 'Software Developer'
+                />
+                <MainContentVideoItem 
+                    thumb = 'thumb2.png'
+                    category = 'Frontend'
+                    title = 'Beginners Guide To Becoming A Professional Frontend Developer'
+                    progress = '50%'
+                    uploaderImage = 'prasant.jpg'
+                    uploaderName = 'Prasant Kumar Singh'
+                    uploaderJob = 'Software Developer'
+                />
+                <MainContentVideoItem 
+                    thumb = 'thumb3.jpg'
+                    category = 'Frontend'
+                    title = 'Beginners Guide To Becoming A Professional Frontend Developer'
+                    progress = '50%'
+                    uploaderImage = 'prasant.jpg'
+                    uploaderName = 'Prasant Kumar Singh'
+                    uploaderJob = 'Software Developer'
+                />
+            </div>
         </div>
      );
 }
