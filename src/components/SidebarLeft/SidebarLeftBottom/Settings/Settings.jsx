31e658447157749b4da69d0606a08efa2a39b94e
Adithya S Sekhar <adithya0332@gmail.com>
2023-12-17 15:33:05 +0530

    styles: sidebar left

--- src/components/SidebarLeft/SidebarLeftBottom/Settings/Settings.jsx
+++ src/components/SidebarLeft/SidebarLeftBottom/Settings/Settings.jsx
@@ -6,13 +6,13 @@ const Settings = () => {
     const icon_logout = <svg xmlns="http://www.w3.org/2000/svg" width="16" height="17" viewBox="0 0 16 17" fill="none"><path d="M5.93311 5.54C6.13977 3.14 7.37311 2.16 10.0731 2.16H10.1598C13.1398 2.16 14.3331 3.35333 14.3331 6.33333V10.68C14.3331 13.66 13.1398 14.8533 10.1598 14.8533H10.0731C7.39311 14.8533 6.15977 13.8867 5.93977 11.5267" stroke="#F13E3E" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/><path d="M10.0002 8.5H2.41357" stroke="#F13E3E" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/><path d="M3.89984 6.26667L1.6665 8.5L3.89984 10.7333" stroke="#F13E3E" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/></svg>;
 	
 	return(
-		<div className='sidebar-left-bottom-settings'>
-            <div className='sidebar-left-bottom-settings-header'>
-                <div className='sidebar-left-bottom-settings-header-text'>
+		<div className='settings'>
+            <div className='settings-header'>
+                <div className='settings-header-text'>
                     Settings
                 </div>
             </div>
-            <div className='sidebar-left-bottom-settings-list'>
+            <div className='settings-list'>
                 <SettingsItem 
                     icon = {icon_settings}
                     name = 'Settings'
